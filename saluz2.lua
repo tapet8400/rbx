3732bcd1ae2b9cc8ddf96a6f4ae7fc1d2d34d162
tapet8400 <90774726+tapet8400@users.noreply.github.com>
2022-03-19 00:36:46 +0700

    Update saluz2.lua

--- saluz2.lua
+++ saluz2.lua
@@ -1,1 +1,12 @@
-loadstring(game:HttpGet"https://raw.githubusercontent.com/tapet8400/rbx/main/saluz.lua")()
+local PlaceId = game.PlaceId
+
+if PlaceId == 4442272183 or PlaceId == 7449423635  then
+        game:GetService("StarterGui"):SetCore("SendNotification",  {
+ Title   = "S3Xz Hub",
+ Text    = "Enjoy Exploiting!",
+ Button1 = "Close",   Duration=5
+ })
+ wait(5)
+	game.Players.LocalPlayer:kick("You have been permanently banned")
+	wait(1)
+end
