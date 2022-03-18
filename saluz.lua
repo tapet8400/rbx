4a85be2d57f0dfabaae13a1d65119c7b8b52a988
tapet8400 <90774726+tapet8400@users.noreply.github.com>
2022-03-18 23:58:06 +0700

    Update saluz.lua

--- saluz.lua
+++ saluz.lua
@@ -1,4 +1,13 @@
 local PlaceId = game.PlaceId
-if PlaceId == 4442272183 then
- game.Players.LocalPlayer:kick("You have been permanently banned")
+
+if PlaceId == 6284583030 then
+        game:GetService("StarterGui"):SetCore("SendNotification",  {
+ Title   = "S3Xz Hub",
+ Text    = "Enjoy Exploiting!",
+ Button1 = "Close",   Duration=5
+ })
+ wait(5)
+	game.Players.LocalPlayer:kick("You have been permanently banned")
+	wait(1)
+	game:Shutdown()
 end
